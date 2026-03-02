6aec355837569daf99023e3a10a924c8a8627c63
Vishal3257 <vt464670@gmail.com>
2026-03-02 10:06:46 +0530

    settings.py changes

--- Genrate_OTP/settings.py
+++ Genrate_OTP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sp43rdxji7&hw_p$@z!ym@opra(3kk3=l3-hl$dw@w3h26i8=8'
+SECRET_KEY = 'django-insecure-sp43rdxji7&hw_p$@z!ym@opra(3kk3=l3-hl$dw@w3h26i8=8)'
 
 AUTH_API_KEY = os.environ.get('AUTH_API_KEY')
 

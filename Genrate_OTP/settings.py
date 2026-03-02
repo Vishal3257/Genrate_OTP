7f7de3b87f10d3ef598e33d640ac06236d13c443
Vishal3257 <vt464670@gmail.com>
2026-03-02 12:43:29 +0530

    Genrate_OTP pycache and settings.py file updates

--- Genrate_OTP/settings.py
+++ Genrate_OTP/settings.py
@@ -157,13 +157,15 @@ REST_FRAMEWORK = {
     ],
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-"""EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vt464670@gmail.com'
-EMAIL_HOST_PASSWORD = 'ubdd izku vcdd mdje'
-os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'Django_OTP_API <vt464670@gmail.com>'"""
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'Django_OTP_API <vt464670@gmail.com>'
+
+#EMAIL_HOST_PASSWORD = 'ubdd izku vcdd mdje'
+

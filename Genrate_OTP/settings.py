dbae50378b0c4f3d6c1bfbe0d366b684836d005c
Vishal3257 <vt464670@gmail.com>
2026-03-03 09:03:36 +0530

    settings email_use_ssl add

--- Genrate_OTP/settings.py
+++ Genrate_OTP/settings.py
@@ -163,6 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'vt464670@gmail.com'
 EMAIL_HOST_PASSWORD = 'ubdd izku vcdd mdje'
 DEFAULT_FROM_EMAIL = 'Django_OTP_API <vt464670@gmail.com>'

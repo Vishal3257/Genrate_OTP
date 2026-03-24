32a57819d3ad7b61703721474bd6862a49a9da82
Vishal3257 <vt464670@gmail.com>
2026-03-24 09:34:17 +0530

    Fixed static files directory and updated settings

--- Genrate_OTP/settings.py
+++ Genrate_OTP/settings.py
@@ -141,7 +141,7 @@ STORAGES = {
 }
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(BASE_DIR, 'staticfiles'), 
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
